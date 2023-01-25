baaecd22475d673dc34d96b71a47c46488393743
lyjeileen <lyjeileen@gmail.com>
2023-01-25 00:07:51 -0800

    Add onClick as props for buttons

--- src/pages/utils.js
+++ src/pages/utils.js
@@ -1,13 +1,36 @@
 import Button from 'components/Button';
+
 export default function Utils() {
   return (
     <div>
       <h2 className="font-bold text-2xl text-amber-800 p-2 my-10">Utils</h2>
       <div>
-        <Button text="Generate content" />
+        <Button
+          text="Generate content"
+          onClick={async () => {
+            await fetch('/api/utils', {
+              body: JSON.stringify({
+                task: 'generate_content',
+              }),
+              headers: { 'Content-Type': 'application/json' },
+              method: 'POST',
+            });
+          }}
+        />
       </div>
       <div>
-        <Button text="Clear database" />
+        <Button
+          text="Clear database"
+          onClick={async () => {
+            await fetch('/api/utils', {
+              body: JSON.stringify({
+                task: 'clear_database',
+              }),
+              headers: { 'Content-Type': 'application/json' },
+              method: 'POST',
+            });
+          }}
+        />
       </div>
     </div>
   );
