510fc520bd45687e3f54c2a7f77e3e4af9df7639
lyjeileen <lyjeileen@gmail.com>
2023-01-28 23:17:39 -0800

    Add Navbar and Head to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <>Hello </>;
+import Head from 'next/head';
+import prisma from 'lib/prisma';
+import { getVideos } from 'lib/data';
+import Navbar from '/components/Navbar';
+
+export default function Home({ videos }) {
+  return (
+    <>
+      <Head>
+        <title>Youtube</title>
+        <meata name="description" content="A great YouTube Clone" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+    </>
+  );
 }
+
+export const getServerSideProps = async () => {
+  let videos = await getVideos({}, prisma);
+  videos = JSON.parse(JSON.stringify(videos));
+  return { props: { videos } };
+};
