5b7e22df3082dae019ccd80e9e8a4777257f8dce
lyjeileen <lyjeileen@gmail.com>
2023-01-29 17:03:52 -0800

    Add css style for videos
    Add grid for different screen sizes

--- components/Videos.js
+++ components/Videos.js
@@ -1,7 +1,12 @@
 import Video from 'components/Video';
 
 export default function Videos({ videos }) {
-  return videos.map((video) => {
+  const videoList = videos.map((video) => {
     return <Video key={video.id} video={video} />;
   });
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {videoList}
+    </div>
+  );
 }
