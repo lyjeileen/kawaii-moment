bb1a7708cc93ab83567639132dffc5c8c4bc3f0f
lyjeileen <lyjeileen@gmail.com>
2023-02-02 00:14:40 -0800

    Add latest videos in sidebar
    -Add options.take for getVideos query
    -Only show 3 latest videos

--- src/pages/video/[id].js
+++ src/pages/video/[id].js
@@ -1,33 +1,41 @@
 import prisma from 'lib/prisma';
-import { getVideo } from 'lib/data';
+import { getVideo, getVideos } from 'lib/data';
 import Avatar from 'components/Avatar';
+import Video from 'components/Video';
 import timeago from 'lib/timeago';
 //lazy load the player for url, reduce main bundle size
 import dynamic from 'next/dynamic';
 const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
-export default function SingleVideo({ video }) {
+export default function SingleVideo({ video, videos }) {
   if (!video) return <p>Video not found</p>;
   const postTime = timeago.format(new Date(video.createdAt));
-
+  const videoList = videos.map((video) => (
+    <Video key={video.id} video={video} />
+  ));
   return (
-    <div className="p-4">
-      <ReactPlayer
-        className="aspect-video"
-        url={video.url}
-        width="100%"
-        height="100%"
-        controls={true}
-        light={video.thumbnail}
-      />
-      <p className="font-bold text-xl mt-2">{video.title}</p>
-      <p className="text-xs text-gray-700">
-        {video.views} views · {postTime}
-      </p>
-      <div className="flex mt-2">
-        <Avatar image={video.author.image} />
-        <p className="text-xl ml-2">{video.author.name}</p>
+    <div className="flex p-4">
+      <div className="w-full md:w-2/3">
+        <div className="w-full aspect-video">
+          <ReactPlayer
+            className="aspect-video"
+            url={video.url}
+            width="100%"
+            height="100%"
+            controls={true}
+            light={video.thumbnail}
+          />
+        </div>
+        <p className="font-bold text-xl mt-2">{video.title}</p>
+        <p className="text-xs text-gray-700">
+          {video.views} views · {postTime}
+        </p>
+        <div className="flex mt-2">
+          <Avatar image={video.author.image} />
+          <p className="text-xl ml-2">{video.author.name}</p>
+        </div>
       </div>
+      <div className="hidden md:block md:w-1/3">{videoList}</div>
     </div>
   );
 }
@@ -35,7 +43,9 @@ export default function SingleVideo({ video }) {
 export const getServerSideProps = async (context) => {
   let video = await getVideo(context.params.id, prisma);
   video = JSON.parse(JSON.stringify(video));
+  let videos = await getVideos({ take: 3 }, prisma);
+  videos = JSON.parse(JSON.stringify(videos));
   return {
-    props: { video },
+    props: { video, videos },
   };
 };
