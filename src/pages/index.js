a2512f67531130b3ff26e23fba5b96ac633e8864
lyjeileen <lyjeileen@gmail.com>
2023-02-03 23:51:33 -0800

    Add load more to home page
    - Correct typo for meta;
    - Add LoadMore
    - Add states for videos and end

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,27 @@
+import { useState } from 'react';
 import Head from 'next/head';
+
 import prisma from 'lib/prisma';
 import { getVideos } from 'lib/data';
-import Videos from '/components/Videos';
 
-export default function Home({ videos }) {
+import Videos from '/components/Videos';
+import LoadMore from '/components/LoadMore';
+
+export default function Home({ initialVideos }) {
+  const [videos, setVideos] = useState(initialVideos);
+  const [end, setEnd] = useState(false);
+
   return (
     <>
       <Head>
         <title>Youtube</title>
-        <meata name="description" content="A great YouTube Clone" />
+        <meta name="description" content="A great YouTube Clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       {videos.length === 0 && <h1>No videos found</h1>}
       <Videos videos={videos} />
+      (!end &&
+      <LoadMore videos={videos} setVideos={setVideos} setEnd={setEnd} />)
     </>
   );
 }
@@ -21,5 +29,6 @@ export default function Home({ videos }) {
 export const getServerSideProps = async () => {
   let videos = await getVideos({}, prisma);
   videos = JSON.parse(JSON.stringify(videos));
-  return { props: { videos } };
+
+  return { props: { initialVideos: videos } };
 };
