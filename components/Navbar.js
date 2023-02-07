7af43cf34cab1d7d2756abe26357bf0d8448f74e
lyjeileen <lyjeileen@gmail.com>
2023-02-06 20:12:52 -0800

    Fix typo for signin path

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,6 +19,7 @@ export default function Navbar() {
       </nav>
     );
   }
+
   return (
     <nav className="bg-amber-50 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="flex flex-wrap items-center justify-between ">
@@ -87,7 +88,7 @@ export default function Navbar() {
             </li>
             <li>
               <a
-                href={session ? '/api/auth/signout' : '/api/auth/signout'}
+                href={session ? '/api/auth/signout' : '/api/auth/signin'}
                 className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 {session ? 'Logout' : 'Login'}
