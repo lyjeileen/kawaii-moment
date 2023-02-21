fee6ab45193bbfccfe0c1d1518d697e9a2efa026
lyjeileen <lyjeileen@gmail.com>
2023-02-20 23:32:14 -0800

    Add message after saving profile

--- src/pages/setup.js
+++ src/pages/setup.js
@@ -11,6 +11,7 @@ export default function Setup() {
   const [name, setName] = useState('');
   const [imageURL, setImageURL] = useState(null);
   const [image, setImage] = useState(null);
+  const [isSaved, setIsSaved] = useState(false);
 
   if (loading) return null;
   if (!session || !session.user)
@@ -31,78 +32,93 @@ export default function Setup() {
     );
 
   return (
-    <div className="m-auto flex flex-col w-fit text-amber-800">
-      <h1 className="font-bold text-xl my-10 text-center">
-        Set up your profile
-      </h1>
-
-      <form
-        className="p-10 pb-4 border-2 border-amber-800 rounded-md"
-        onSubmit={async (e) => {
-          e.preventDefault();
-
-          const body = new FormData();
-          body.append('image', image);
-          body.append('name', name);
-
-          await fetch('/api/setup', {
-            body,
-            method: 'POST',
-          });
-
-          session.user.name = name;
-          session.user.image = image;
-        }}
-      >
-        <div className="my-2">Name</div>
-        <input
-          type="text"
-          name="name"
-          required
-          value={name}
-          className="border p-1 border-amber-800 rounded-md"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label className="cursor-pointer">
-          <div className="my-2">Avatar</div>
-          <div className="border p-1 border-amber-800 rounded-md bg-white">
-            {!imageURL ? (
-              <p>Upload image</p>
-            ) : (
-              <Image
-                className="rounded-full h-8"
-                src={imageURL}
-                alt="Image"
-                width={32}
-                height={32}
-              />
-            )}
-
-            <input
-              name="image"
-              type="file"
-              accept="image/*"
-              className="hidden"
-              required
-              onChange={(event) => {
-                if (event.target.files && event.target.files[0]) {
-                  if (event.target.files[0].size > 3072000) {
-                    alert('Maximum size allowed is 3MB');
-                    return false;
-                  }
-
-                  setImage(event.target.files[0]);
-                  //URL.createObjectURL return a string containing an object URL that can be used to reference the contents of the specified source object
-                  setImageURL(URL.createObjectURL(event.target.files[0]));
-                }
-              }}
-            />
-          </div>
-        </label>
-        <div className="my-6 text-center">
-          <Button text="Save" />
+    <>
+      {isSaved && (
+        <div
+          className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 m-auto w-fit"
+          role="alert"
+        >
+          <p class="font-bold">You have saved your profile successfully!</p>
         </div>
-      </form>
-    </div>
+      )}
+      <div className="m-auto flex flex-col w-fit text-amber-800">
+        <h1 className="font-bold text-xl my-10 text-center">
+          Set up your profile
+        </h1>
+
+        <form
+          className="p-10 pb-4 border-2 border-amber-800 rounded-md"
+          onSubmit={async (e) => {
+            e.preventDefault();
+
+            //clear previous banner
+            setIsSaved(false);
+
+            const body = new FormData();
+            body.append('image', image);
+            body.append('name', name);
+
+            await fetch('/api/setup', {
+              body,
+              method: 'POST',
+            });
+
+            setIsSaved(true);
+
+            session.user.name = name;
+            session.user.image = image;
+          }}
+        >
+          <div className="my-2">Name</div>
+          <input
+            type="text"
+            name="name"
+            required
+            value={name}
+            className="border p-1 border-amber-800 rounded-md"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label className="cursor-pointer">
+            <div className="my-2">Avatar</div>
+            <div className="border p-1 border-amber-800 rounded-md bg-white">
+              {!imageURL ? (
+                <p>Upload image</p>
+              ) : (
+                <Image
+                  className="rounded-full h-8"
+                  src={imageURL}
+                  alt="Image"
+                  width={32}
+                  height={32}
+                />
+              )}
+
+              <input
+                name="image"
+                type="file"
+                accept="image/*"
+                className="hidden"
+                required
+                onChange={(event) => {
+                  if (event.target.files && event.target.files[0]) {
+                    if (event.target.files[0].size > 3072000) {
+                      alert('Maximum size allowed is 3MB');
+                      return false;
+                    }
+
+                    setImage(event.target.files[0]);
+                    //URL.createObjectURL return a string containing an object URL that can be used to reference the contents of the specified source object
+                    setImageURL(URL.createObjectURL(event.target.files[0]));
+                  }
+                }}
+              />
+            </div>
+          </label>
+          <div className="my-6 text-center">
+            <Button text="Save" />
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
