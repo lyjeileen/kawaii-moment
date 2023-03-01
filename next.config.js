ac286c1d7944fbf969f3b28058166c2790e36e06
lyjeileen <lyjeileen@gmail.com>
2023-03-01 00:21:57 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     domains: [
       'localhost',
       'lyjeileen.s3.ca-central-1.amazonaws.com',
+      'lyjeileen.s3.amazonaws.com',
       'loremflickr.com',
       'cloudflare-ipfs.com',
     ],
