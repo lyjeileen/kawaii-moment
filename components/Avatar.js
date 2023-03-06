d485aef0f5a90b594a1b383438eced93a811ed8d
lyjeileen <lyjeileen@gmail.com>
2023-03-05 22:47:55 -0800

    Adjust navbar
    -Fix avatar size issue
    -Change css style

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 export default function Avatar({ image }) {
   if (image) {
     return (
-      <div>
+      <div className="h-min">
         <Image
-          className="rounded-full"
+          className="rounded-full w-10 h-10"
           src={image}
           alt="Author name"
           width={40}
@@ -14,11 +14,12 @@ export default function Avatar({ image }) {
       </div>
     );
   }
+
   return (
     //show a placeholder icon for the user profile when there is no custom image available
     <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 ">
       <svg
-        className="absolute w-12 h-12 text-gray-400 -left-1"
+        className="absolute w-10 h-10 text-gray-400 -left-1"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
--- components/Navbar.js
+++ components/Navbar.js
@@ -54,7 +54,7 @@ export default function Navbar() {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-amber-50 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 mt-4 items-center md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-amber-50 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Upload />
             </li>
@@ -86,7 +86,7 @@ export default function Navbar() {
               <li>
                 <Link
                   href={`/channel/${session.user.name}`}
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block p-2 rounded-full hover:ring-2 hover:ring-amber-500 md:p-0"
                 >
                   <Avatar image={session.user.image} />
                 </Link>
